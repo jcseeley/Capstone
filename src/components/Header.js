dba299d3404ded80beed7cea2aaa5107634e0afd
Jase Seeley <jcseeley31590@yahoo.com>
2022-05-06 14:51:48 -0700

    WIP: still tinkering with home page and color scheme.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
-import Logo from "../img/HowlandBigger.svg";
+import Logo from "../img/HowlandTransparent.png";
 
 function Header(){
   return (
     <React.Fragment>
-      <div className="flex">
-      <div className="flex w-3/4 items-center justify-left">
-        <img className="h-48 border-2 border-solid border-orange-900 mt-2 mb-2 ml-2 mr-2" src={Logo} alt="Howland Management Logo" />
-      </div>
+      <div className="flex justify-center shadow-md">
+        <div className="flex w-11/12">
+          <div className="flex w-3/4 items-center justify-left">
+            <img className="h-40 border-2 border-solid border-orange-900 mt-2 mb-2" src={Logo} alt="Howland Management Logo" />
+          </div>
+        </div>
       </div>
       <hr />
     </React.Fragment>
--- src/components/PropertyList.js
+++ src/components/PropertyList.js
@@ -8,7 +8,7 @@ function PropertyList(props) {
   return (
     <React.Fragment>
       <div className="grid grid-cols-2">
-        <div className='justify-self-center border-2 border-solid border-orange-900 text-center w-3/4 ml-2 mt-2 mr-2 mb-2'>
+        <div className='shadow-2xl justify-self-center text-center w-5/6 ml-2 mt-4 mr-2 mb-4 px-2'>
           <h1 className="text-xl font-bold text-orange-900 mb-2 mt-2">Escalante, Utah</h1>
           <Carousel animationHandler="fade" autoPlay={true} infiniteLoop={true} swipeable={false}>
             <div>
@@ -27,29 +27,28 @@ function PropertyList(props) {
               <img src="https://a0.muscache.com/im/pictures/miso/Hosting-13466741/original/d27a015c-0ca7-4059-aef0-8021e5f392df.jpeg?im_w=720" alt="test"/>
             </div>
           </Carousel>
+          <p className="mb-2 ml-2 mr-2">This river front home tucked in between large cottonwood shade trees with views on all sides of the Escalante Canyon, meadows, cliffs, and river. Hike from the front door to first class scenic wonders. There are natural wonders right out the front door and within an hour's drive. Look for deer and wild turkey in the meadow in the mornings and evenings and watch the cloud shadows change over the canyon walls. Head up or down the canyon into rugged wilderness, and return home to comfort.</p>
           <div className="flex justify-center">
-            <button className="text-orange-900 border-2 border-solid border-orange-900 bg-orange-300 hover:bg-orange-200 font-bold py-2 px-4 rounded-full ml-2 mr-2 mb-2" onClick={() => props.onPropertySelection("Utah")}>View Utah Rentals</button>
+            <button className="text-orange-900 border-2 border-solid border-orange-900 bg-transparent hover:bg-orange-300 font-bold py-2 px-4 rounded ml-2 mr-2 mb-2" onClick={() => props.onPropertySelection("Utah")}>View Utah Rentals</button>
           </div>
         </div>
 
-        <div className='justify-self-center text-center w-3/4 ml-2 mt-2 mr-2'>
-          <h1 className="text-xl font-bold text-orange-900">Sedona, Arizona</h1>
+        <div className='shadow-2xl justify-self-center text-center w-5/6 ml-2 mt-4 mr-2 mb-4 px-2'>
+          <h1 className="text-xl font-bold mb-2 mt-2 text-orange-900">Sedona, Arizona</h1>
           <Carousel animationHandler="fade" autoPlay={true} infiniteLoop={true} swipeable={false}>
             <div>
               <img src="https://a0.muscache.com/im/pictures/fbb24b31-ea14-4677-9832-47623f2e6be8.jpg?im_w=720" alt="test"/>
-              <p className="legend">Legend 1</p>
             </div>
             <div>
               <img src="https://a0.muscache.com/im/pictures/miso/Hosting-21255496/original/ce2b213d-d1b5-4872-8fc5-70490e32c4b0.jpeg?im_w=1200" alt="test"/>
-              <p className="legend">Legend 2</p>
             </div>
             <div>
               <img src="https://a0.muscache.com/im/pictures/miso/Hosting-21255496/original/965ca84d-4a42-47aa-b9fd-ed100c45f417.jpeg?im_w=1200" alt="test"/>
-              <p className="legend">Legend 3</p>
             </div>
           </Carousel>
+          <p className="text-left mb-2 ml-2 mr-2">This Arts and Crafts inspired home has been our personal residence for 30 years. The home's exterior is Japanese/Southwest inspired, set in a lush oasis of mature landscaping and green lawns. Great hiking begins just minutes from the front door. In this tranquility, we see an abundance of local wildlife, including javalina, quail, and even the occasional bobcat. We are also just a few minutes away from the gourmet restaurants, spas, galleries, and bike rentals of the Village of Oak Creek.</p>
           <div className="flex justify-center">  
-            <button className="text-orange-900 border-2 border-solid border-orange-900 bg-orange-300 hover:bg-orange-200 font-bold py-2 px-4 rounded-full ml-2 mr-2 mb-2" onClick={() => props.onPropertySelection("Sedona")}>View Sedona Rentals</button>
+            <button className="text-orange-900 border-2 border-solid border-orange-900 bg-transparent hover:bg-orange-300 font-bold py-2 px-4 rounded ml-2 mr-2 mb-2" onClick={() => props.onPropertySelection("Sedona")}>View Sedona Rentals</button>
           </div>
         </div>
       </div>
