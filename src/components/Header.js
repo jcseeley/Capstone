82a8fd6ed2e4a9d957972d1c4e80bc44c1e16aab
Jase Seeley <jcseeley31590@yahoo.com>
2022-05-13 08:41:48 -0700

    WIP: Almost done with home page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header(){
       {/* <div className="flex justify-center shadow-md">
         <div className="flex w-11/12"> */}
           <div className="flex items-center justify-center">
-            <img className="h-48 border-2 border-solid border-orange-900 mt-2 mb-2" src={Logo} alt="Howland Management Logo" />
+            <img className="h-52 border-2 border-solid border-orange-900 mt-2 mb-2" src={Logo} alt="Howland Management Logo" />
           </div>
         {/* </div>
       </div> */}
--- src/components/PropertyList.js
+++ src/components/PropertyList.js
@@ -8,8 +8,7 @@ function PropertyList(props) {
   return (
     <React.Fragment>
       <div className="grid grid-cols-2">
-        <div className='shadow-2xl justify-self-center text-center w-5/6 ml-2 mt-4 mr-2 mb-4 px-2'>
-          <h1 className="text-xl font-bold text-orange-900 mb-2 mt-2">Escalante, Utah</h1>
+        <div className='justify-self-center text-center w-5/6 ml-2 mt-4 mr-2 mb-4 px-2'>
           <Carousel animationHandler="fade" autoPlay={true} infiniteLoop={true} swipeable={false}>
             <div>
               <img src="https://a0.muscache.com/im/pictures/1f966431-d498-4afc-94ac-e2ef6e2549c7.jpg?im_w=720" alt="test"/>
@@ -27,14 +26,16 @@ function PropertyList(props) {
               <img src="https://a0.muscache.com/im/pictures/miso/Hosting-13466741/original/d27a015c-0ca7-4059-aef0-8021e5f392df.jpeg?im_w=720" alt="test"/>
             </div>
           </Carousel>
-          <p className="text-orange-900 mb-2 ml-2 mr-2">This river front home tucked in between large cottonwood shade trees with views on all sides of the Escalante Canyon, meadows, cliffs, and river. Hike from the front door to first class scenic wonders. There are natural wonders right out the front door and within an hour's drive. Look for deer and wild turkey in the meadow in the mornings and evenings and watch the cloud shadows change over the canyon walls. Head up or down the canyon into rugged wilderness, and return home to comfort.</p>
+          <div className="rounded-3xl bg-orange-200 text-orange-900">
+          <h1 className="text-xl font-bold text-orange-900 mb-2 pt-2">Escalante, Utah</h1>
+          <p className="mb-4 ml-2 mr-2 px-4">This river front home tucked in between large cottonwood shade trees with views on all sides of the Escalante Canyon, meadows, cliffs, and river. Hike from the front door to first class scenic wonders. There are natural wonders right out the front door and within an hour's drive. Look for deer and wild turkey in the meadow in the mornings and evenings and watch the cloud shadows change over the canyon walls. Head up or down the canyon into rugged wilderness, and return home to comfort.</p>
           <div className="flex justify-center">
-            <button className="text-orange-900 border-2 border-solid border-orange-900 bg-transparent hover:bg-orange-300 font-bold py-2 px-4 rounded ml-2 mr-2 mb-2" onClick={() => props.onPropertySelection("Utah")}>View Utah Rentals</button>
+            <button className="block text-orange-900 border-2 border-solid border-orange-900 bg-orange-300 hover:bg-orange-100 font-bold py-2 px-4 rounded ml-2 mr-2 mb-4" onClick={() => props.onPropertySelection("Utah")}>View Utah Rentals</button>
+          </div>
           </div>
         </div>
 
-        <div className='shadow-2xl justify-self-center text-center w-5/6 ml-2 mt-4 mr-2 mb-4 px-2'>
-          <h1 className="text-xl font-bold mb-2 mt-2 text-orange-900">Sedona, Arizona</h1>
+        <div className='justify-self-center text-center w-5/6 ml-2 mt-4 mr-2 mb-4 px-2'>
           <Carousel animationHandler="fade" autoPlay={true} infiniteLoop={true} swipeable={false}>
             <div>
               <img src="https://a0.muscache.com/im/pictures/fbb24b31-ea14-4677-9832-47623f2e6be8.jpg?im_w=720" alt="test"/>
@@ -46,9 +47,12 @@ function PropertyList(props) {
               <img src="https://a0.muscache.com/im/pictures/miso/Hosting-21255496/original/965ca84d-4a42-47aa-b9fd-ed100c45f417.jpeg?im_w=1200" alt="test"/>
             </div>
           </Carousel>
-          <p className="text-left mb-2 ml-2 mr-2">This Arts and Crafts inspired home has been our personal residence for 30 years. The home's exterior is Japanese/Southwest inspired, set in a lush oasis of mature landscaping and green lawns. Great hiking begins just minutes from the front door. In this tranquility, we see an abundance of local wildlife, including javalina, quail, and even the occasional bobcat. We are also just a few minutes away from the gourmet restaurants, spas, galleries, and bike rentals of the Village of Oak Creek.</p>
+          <div className="rounded-3xl bg-red-200 text-orange-900">
+          <h1 className="text-xl font-bold text-orange-900 mb-2 pt-2">Sedona, Arizona</h1>
+          <p className="mb-4 ml-2 mr-2 px-4">This Arts and Crafts inspired home has been our personal residence for 30 years. The home's exterior is Japanese/Southwest inspired, set in a lush oasis of mature landscaping and green lawns. Great hiking begins just minutes from the front door. In this tranquility, we see an abundance of local wildlife, including javalina, quail, and even the occasional bobcat. We are also just a few minutes away from the gourmet restaurants, spas, galleries, and bike rentals of the Village of Oak Creek.</p>
           <div className="flex justify-center">  
-            <button className="text-orange-900 border-2 border-solid border-orange-900 bg-transparent hover:bg-orange-300 font-bold py-2 px-4 rounded ml-2 mr-2 mb-2" onClick={() => props.onPropertySelection("Sedona")}>View Sedona Rentals</button>
+            <button className="block text-orange-900 border-2 border-solid border-orange-900 bg-red-300 hover:bg-red-100 font-bold py-2 px-4 rounded ml-2 mr-2 mb-4" onClick={() => props.onPropertySelection("Sedona")}>View Sedona Rentals</button>
+          </div>
           </div>
         </div>
       </div>
