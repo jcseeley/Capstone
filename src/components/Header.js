cc03f2f6dc85978d2b0fb734d6ff4834bc0611c1
Jase Seeley <jcseeley31590@yahoo.com>
2022-05-13 12:34:00 -0700

    WIP: Still finalizing design.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Logo from "../img/HowlandTransparent.png";
+import Logo from "../img/Howland2Suns.png";
 
 function Header(){
   return (
     <React.Fragment>
-      <div className="flex justify-center">
-        <div className="flex w-11/12">
-          <div className="flex items-center justify-left">
-            <img className="w-1/4 border-2 border-solid border-orange-900 mt-2 mb-2" src={Logo} alt="Howland Management Logo" />
+      <div id="header" className="flex justify-center">
+        {/* <div className="flex w-11/12"> */}
+          <div className="flex items-center justify-center">
+            <img className="w-5/6" src={Logo} alt="Howland Management Logo" />
           </div>
-        </div>
+        {/* </div> */}
       </div>
     </React.Fragment>
   );
